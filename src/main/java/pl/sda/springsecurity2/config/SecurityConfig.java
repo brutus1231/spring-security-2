834b3b0eef12511eb04c53d83eb1b0d5eb8ca346
Zdzislaw Trzaska <ztrzaska2@gmail.com>
2021-07-24 14:00:16 +0200

    rejestracja

--- src/main/java/pl/sda/springsecurity2/config/SecurityConfig.java
+++ src/main/java/pl/sda/springsecurity2/config/SecurityConfig.java
@@ -1,11 +1,14 @@
 package pl.sda.springsecurity2.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 import javax.sql.DataSource;
 
@@ -17,6 +20,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
     DataSource dataSource;
+
+    @Bean
+    public PasswordEncoder encoder() {
+        return new BCryptPasswordEncoder();
+    }
 
     @Autowired
     public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
--- src/main/java/pl/sda/springsecurity2/controller/RegistrationController.java
+++ src/main/java/pl/sda/springsecurity2/controller/RegistrationController.java
@@ -2,6 +2,9 @@ package pl.sda.springsecurity2.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import pl.sda.springsecurity2.model.UserEntity;
 
 @Controller
 public class RegistrationController {
@@ -9,5 +12,10 @@ public class RegistrationController {
     @GetMapping("/register")
     public String view() {
         return "registration";
+    }
+
+    @PostMapping("/register")
+    public String createUser(@ModelAttribute("user") UserEntity user) {
+        return "login";
     }
 }
